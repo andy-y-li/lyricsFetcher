61586b57f95e4d52356b9c487d107a22137aabd7
liaogang <ynwlgh@icloud.com>
2015-01-23 09:25:00 +0600

    add play file

--- src/audioTag.cpp
+++ src/audioTag.cpp
@@ -1,6 +1,6 @@
 #include <string.h>
 #include <taglib/taglib.h> 
-#include <taglib/id3v2tag.h> 
+#include <taglib/id3v2tag.h>
 #include <taglib/id3v2frame.h>
 #include <taglib/id3v2header.h>
 #include <taglib/mpegfile.h>
@@ -63,26 +63,6 @@ bool getId3Info(const char * filename , char *artist , char * title  )
         {
             strcpy(artist,id3v1tag->artist().toCString(true));
             strcpy(title,id3v1tag->title().toCString(true));
-            
-            //the id3v1tag string may be wrongly
-            //like Œ“∫‹∫√0000fsadfsdf...
-           // title=id3v1tag->title().toWString();
-           // artist=id3v1tag->artist().toWString();
-           // album=id3v1tag->album().toWString();
-           // genre=id3v1tag->genre().toWString();
-            
-           // TCHAR cYear[MAX_PATH]={_T("?")};
-//            uYear=id3v1tag->year();
-//            if(uYear!=0)
-//                _itow(id3v1tag->year(),cYear,10);
-//            year=cYear;
-//
-            
-            //to fix
-//            TrimRightByNull(title);
-//            TrimRightByNull(artist);
-//            TrimRightByNull(album);
-//            TrimRightByNull(genre);
         }
     }
     
@@ -93,10 +73,6 @@ bool getId3Info(const char * filename , char *artist , char * title  )
         //without suffix
         if (strlen(title))
         {
-//            int a=url.find_last_of('\\');
-//            int b=url.find_last_of('.');
-//            if (a<b && b!=url.npos)
-//                title=url.substr(a+1,b-(a+1));
         }
     }
     
